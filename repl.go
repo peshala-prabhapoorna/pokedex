94d1c5dbf647e62a838f21e85922f67c9d2b95cb
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2025-04-30 09:51:35 +0530

    feat: add repl

--- repl.go
+++ repl.go
@@ -1,6 +1,28 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		input := scanner.Text()
+		words := cleanInput(input)
+		if len(words) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	textLowerCase := strings.ToLower(text)
